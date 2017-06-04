dd6fc70cd758c5b76a77e176ceee7e1188ef5c45
PukaPy <pukalordu@yahoo.com>
2017-06-04 21:30:13 +0300

    Update h1z1Leader.py

--- h1z1Leader.py
+++ h1z1Leader.py
@@ -6,6 +6,7 @@ __author__ = "PukaPy"
 class GetRank:
 
     def __init__(self, nameX, region):
+        self.url = 'https://census.daybreakgames.com/rest/leaderboard/kotk/name-search-get-page'
         self.nameX = nameX
         self.region = region
         self.carrot = []
@@ -46,9 +47,47 @@ class GetRank:
             "name": "{}".format(nameX)
         }
 
-        self.url = 'https://census.daybreakgames.com/rest/leaderboard/kotk/name-search-get-page'
+        session = requests.session()
+        p = session.post(
+            self.url,
+            params=self.params,
+            headers=self.headers
+        )
+        self.last_j = p.json()
+        if self.last_j['successPayload']['totalPages'] == 0:
+            raise NameError("User not found!")
+    def get_user_name(self):
+        """
+        Returns the user_name
+        :return:
+        """
+        return self.last_j["successPayload"]["rows"][0]["values"]["user_name"]
 
-    def getRegions(self):
+    def get_game_user_id(self):
+        """
+        Returns the game_user_id
+        :return:
+        """
+        return self.last_j["successPayload"]["rows"][0]["values"]['game_user_id']
+
+    def store_game_user_id(self):
+        """
+        Stores the game_user_id in a file
+        :return:
+        """
+        wwrite = "{} - {}\n".format(
+                self.get_game_user_id(),
+                self.get_user_name()
+            )
+        with open('game_user_id_logs.txt', 'ab+') as f:
+            l = f.readlines()
+            if wwrite in l:
+                print "User allready in file!"
+                return "User allready in file!"
+            f.write(wwrite)
+        f.close()
+
+    def get_regions(self):
         """
         Get the id for every region
         :return:
@@ -56,7 +95,8 @@ class GetRank:
         for k, v in self.regions.items():
             print "Region: {} With ID -> {}".format(k, v)
 
-    def PrintShiny(self, Scores):
+
+    def print_shiny(self, Scores):
         if len(Scores["score"]) < 6:
             self.carrot.append("Score:   {:,} > ".format(
                 int(Scores["score"])
@@ -92,23 +132,11 @@ class GetRank:
           Wins per match ratio, etc..
         :return:
         """
-
-        session = requests.session()
-        p = session.post(
-            self.url,
-            params=self.params,
-            headers=self.headers
-        )
-        last_j = p.json()
-
+        self.store_game_user_id()
         # Assign the current Rank ex(1,2,3..6) to rank variable
         # and the sub-rank to the sub_rank variable
-        try:
-            rank = last_j["successPayload"]["rows"][0]["values"]["tier"]
-            sub_rank = last_j["successPayload"]["rows"][0]["values"]["subtier"]
-        except IndexError:
-            print "User not found!"
-            quit()
+        rank = self.last_j["successPayload"]["rows"][0]["values"]["tier"]
+        sub_rank = self.last_j["successPayload"]["rows"][0]["values"]["subtier"]
 
         #
         # Get player rank
@@ -120,16 +148,16 @@ class GetRank:
         # Print name, position
         #
         print "\n\nUser : {}\n".format(
-            last_j["successPayload"]["rows"][0]["values"]["user_name"]
+            self.last_j["successPayload"]["rows"][0]["values"]["user_name"]
         ), "\nIn position {:,}\n-----------------\nTop 10 Matches!\n-----------------".format(
-            int(last_j["successPayload"]["rows"][0]["position"])
+            int(self.last_j["successPayload"]["rows"][0]["position"])
         )
         #
         # Print top10 matches
         #
         for i in range(0, 10):
-            scores = last_j["successPayload"]["rows"][0]["detail"]["top_matches"][i]
-            self.PrintShiny(scores)
+            scores = self.last_j["successPayload"]["rows"][0]["detail"]["top_matches"][i]
+            self.print_shiny(scores)
 
         print "-----------------\r\n"
         #
@@ -145,14 +173,14 @@ class GetRank:
               "Total Kills: {:,}\n" \
               "\n--> RANK: {} {} <--\n"\
             .format(
-                float(last_j["successPayload"]["rows"][0]["values"]["kills_per_match"]),
-                float(last_j["successPayload"]["rows"][0]["values"]["wins_per_match"]),
-                float(last_j["successPayload"]["rows"][0]["values"]["top_tens_per_match"]),
-                int(last_j["successPayload"]["rows"][0]["values"]["top_10_total_score"]),
-                int(last_j["successPayload"]["rows"][0]["values"]["top_kills"]),
-                int(last_j["successPayload"]["rows"][0]["values"]["total_matches"]),
-                int(last_j["successPayload"]["rows"][0]["values"]["total_wins"]),
-                int(last_j["successPayload"]["rows"][0]["values"]["total_kills"]),
+                float(self.last_j["successPayload"]["rows"][0]["values"]["kills_per_match"]),
+                float(self.last_j["successPayload"]["rows"][0]["values"]["wins_per_match"]),
+                float(self.last_j["successPayload"]["rows"][0]["values"]["top_tens_per_match"]),
+                int(self.last_j["successPayload"]["rows"][0]["values"]["top_10_total_score"]),
+                int(self.last_j["successPayload"]["rows"][0]["values"]["top_kills"]),
+                int(self.last_j["successPayload"]["rows"][0]["values"]["total_matches"]),
+                int(self.last_j["successPayload"]["rows"][0]["values"]["total_wins"]),
+                int(self.last_j["successPayload"]["rows"][0]["values"]["total_kills"]),
                 rank, sub_rank
             )
 
